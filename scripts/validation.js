f1c91ac6caf563b75caa6cca911c8d3454cbc056
Marisol Flores <mariflores5877@gmail.com>
2026-02-24 06:35:18 -0600

    Add toggleButtonState and hasInvalidInput

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,25 @@
+const toggleButtonState = (spotsNputList, button) => {
+  const hasInvalidInput = spotsNputList.some(input => !input.validity.valid);
+  button.disabled = hasInvalidInput;
+};
+
+
 const setEventListeners = (formLmnt) => {
   const spotsNputList = Array.from(formLmnt.querySelectorAll(".modal__input"));
   const sbmtBtn = formLmnt.querySelector(".modal__submit-btn");
+};
+
+toggleButtonState(spotsNputList, sbmtBtn);
+
+spotsNputList.forEach(nputLement) => {
+
+
+  nputLement.addEventListener("change", () => {
+    checkInputValidity(formLmnt, nputLement);
+    toggleButtonState(spotsNputList, sbmtBtn);
+
+
+  }); //don't commit this just yet. //
 };
 
 const enableValidation = () => {
