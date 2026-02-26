d41b94ef6de9e6980301acb376db20c2b04b6add
Marisol Flores <mariflores5877@gmail.com>
2026-02-26 05:05:26 -0600

    Add input and blur event listeners

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,32 +1,35 @@
-const toggleButtonState = (spotsNputList, button) => {
-  const hasInvalidInput = spotsNputList.some(input => !input.validity.valid);
-  button.disabled = hasInvalidInput;
+const checkInputValidity = (formLmnt, nputLement) => {
+  console.log(nputLement.validity);
 };
 
+const toggleButtonState = (spotsNputList, button) => {
+  const hasInvalidInput = spotsNputList.some((input) => !input.validity.valid);
+  button.disabled = hasInvalidInput;
+};
 
 const setEventListeners = (formLmnt) => {
   const spotsNputList = Array.from(formLmnt.querySelectorAll(".modal__input"));
   const sbmtBtn = formLmnt.querySelector(".modal__submit-btn");
+
+  toggleButtonState(spotsNputList, sbmtBtn);
+
+  spotsNputList.forEach((nputLement) => {
+    nputLement.addEventListener("input", () => {
+      checkInputValidity(formLmnt, nputLement);
+      toggleButtonState(spotsNputList, sbmtBtn);
+    });
+    nputLement.addEventListener("blur", () => {
+      checkInputValidity(formLmnt, nputLement);
+      toggleButtonState(spotsNputList, sbmtBtn);
+    });
+  });
 };
 
-toggleButtonState(spotsNputList, sbmtBtn);
-
-spotsNputList.forEach(nputLement) => {
-
-
-  nputLement.addEventListener("change", () => {
-    checkInputValidity(formLmnt, nputLement);
-    toggleButtonState(spotsNputList, sbmtBtn);
-
-
-  }); //don't commit this just yet. //
-};
-
-const enableValidation = () => {
+function enableValidation() {
   const spotsFormList = Array.from(document.querySelectorAll(".modal__form"));
   spotsFormList.forEach((formLmnt) => {
     setEventListeners(formLmnt);
   });
-};
+}
 
 enableValidation();
